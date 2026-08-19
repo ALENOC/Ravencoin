344a0f8560c08374b3343dc0e2c1af0b11fc12a6
Alessandro Nocentini <185200505+ALENOC@users.noreply.github.com>
2026-08-19 23:38:42 +0200

    depends: use cross-aware CMake wrapper for liboqs

--- depends/packages/liboqs.mk
+++ depends/packages/liboqs.mk
@@ -5,24 +5,28 @@ $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=df999915204eb1eba311d89e83d1edd3a514d5a07374745d6a9e5b2dd0d59c08
 $(package)_dependencies=
 $(package)_patches=
+$(package)_build_subdir=build
 
 define $(package)_set_vars
   $(package)_config_opts=-DOQS_BUILD_ONLY_LIB=ON
   $(package)_config_opts+=-DOQS_MINIMAL_BUILD="SIG_ml_dsa_44"
   $(package)_config_opts+=-DOQS_USE_OPENSSL=OFF
   $(package)_config_opts+=-DBUILD_SHARED_LIBS=OFF
-  $(package)_config_opts+=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
   $(package)_config_opts+=-DOQS_DIST_BUILD=ON
 endef
 
+define $(package)_preprocess_cmds
+  mkdir -p build
+endef
+
 define $(package)_config_cmds
-  cmake -S . -B build $($(package)_config_opts)
+  $($(package)_cmake) .. $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
-  cmake --build build --parallel
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
-  cmake --install build --prefix $($(package)_staging_prefix_dir)
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
